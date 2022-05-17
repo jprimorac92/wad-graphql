e17d0f6615f9cdf6c1e9f5456bdcfb78afebac45
jprimorac92 <josip.primorac@ecx.io>
2022-05-17 21:19:15 +0200

    Setup wad-demo project with Spring initializer.

---
+++ wad-demo/src/main/java/ix/ibm/waddemo/WadDemoApplication.java
@@ -0,0 +1,13 @@
+package ix.ibm.waddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WadDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WadDemoApplication.class, args);
+	}
+
+}
---
+++ wad-demo/src/test/java/ix/ibm/waddemo/WadDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package ix.ibm.waddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WadDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
