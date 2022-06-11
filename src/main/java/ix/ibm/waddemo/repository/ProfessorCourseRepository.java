41bed67e18832f43863b9c316559748fd3a8e1c3
jprimorac92 <josip.primorac@ecx.io>
2022-06-11 14:58:13 +0200

    Prepare app for deploy on heroku

--- src/main/java/ix/ibm/waddemo/repository/ProfessorCourseRepository.java
+++ src/main/java/ix/ibm/waddemo/repository/ProfessorCourseRepository.java
@@ -1,14 +1,14 @@
 package ix.ibm.waddemo.repository;
 
-import java.util.List;
+import ix.ibm.waddemo.pojo.ProfessorCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import ix.ibm.waddemo.pojo.ProfessorCourse;
+import java.util.List;
 
 @Repository
-public interface ProfessorCourseRepository extends JpaRepository<ProfessorCourse,Long>
-{
+public interface ProfessorCourseRepository extends JpaRepository<ProfessorCourse, Long> {
     List<ProfessorCourse> findAllByProfessorId(Long professorId);
+
     List<ProfessorCourse> findAllByCourseId(Long courseId);
 }
--- src/main/java/ix/ibm/waddemo/resolver/CourseResolver.java
+++ src/main/java/ix/ibm/waddemo/resolver/CourseResolver.java
@@ -1,27 +1,20 @@
 package ix.ibm.waddemo.resolver;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.coxautodev.graphql.tools.GraphQLResolver;
 import ix.ibm.waddemo.pojo.Course;
 import ix.ibm.waddemo.pojo.Professor;
-import ix.ibm.waddemo.service.CourseService;
 import ix.ibm.waddemo.service.ProfessorService;
 
-public class CourseResolver implements GraphQLResolver<Course>
-{
+import java.util.List;
+
+public class CourseResolver implements GraphQLResolver<Course> {
     private final ProfessorService professorService;
 
-    public CourseResolver(ProfessorService professorService)
-    {
+    public CourseResolver(ProfessorService professorService) {
         this.professorService = professorService;
     }
 
-    public List<Professor> professors(Course course)
-    {
+    public List<Professor> professors(Course course) {
         return professorService.findAllProfessorsForCourse(course);
     }
 }
